aa9956354bedb0fa339c71f3f427dd1e2a4d1351
Gabrielle Monteiro <54419321+gaabemonteiro@users.noreply.github.com>
2021-10-14 20:44:02 -0300

    Exercício 3
    contagem de letras, colocar em maiúscula ou minúscula

---
+++ aula3.js
@@ -0,0 +1,11 @@
+
+var nome = window.prompt('Digite seu nome:')
+
+        //contar quantos caracteres a string tem
+        document.write(`Olá, ${nome}, seu nome tem ${nome.length} letras.<br/>`)
+
+        //colocar tudo em maiúsculo
+        document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()} <br/>`)
+
+        //colocar tudo em minúsculo
+        document.write(`Seu nome em minúsculo é ${nome.toLowerCase()}`)
