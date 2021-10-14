80ca8654853ffda51cfe44dc12e5dfd684d47b80
Gabrielle Monteiro <54419321+gaabemonteiro@users.noreply.github.com>
2021-10-14 20:31:39 -0300

    Exercício 1 de javascript
    Comandos básicos: alert e window.prompt

---
+++ aula1.js
@@ -0,0 +1,8 @@
+//Mostra apenas a mensagem e o botão de OK
+window.alert('Minha primeira mensagem')
+
+//Mostra a mensagem, o botão de OK e de CANCEL
+window.confirm('Está gostando de JS?')
+
+//Além de mostrar a mensagem, abre um espaço para o usuário digitar
+window.prompt('Qual seu nome?')
