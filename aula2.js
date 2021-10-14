02b6e83248967d0de84a12bf29f076db35af0c8f
Gabrielle Monteiro <54419321+gaabemonteiro@users.noreply.github.com>
2021-10-14 20:37:48 -0300

    Soma simples e template string
    guardar dados, mostrar utilizando o document.write e converter string para número

---
+++ aula2.js
@@ -0,0 +1,6 @@
+//É preciso converter de string para o número. Para isso, utilizar o Number na frente.
+var n1 = Number(window.prompt('Digite o primeiro número:'))
+var n2 = Number(window.prompt('Digite o segundo número:'))
+
+var soma = n1 + n2
+document.write(`A soma de ${n1} e ${n2} é ${soma}`)
